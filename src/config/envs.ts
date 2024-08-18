0716d3a8dc8f1c775a4ea1ba542404fdb7388b8b
Adrian <alextravc.342@gmail.com>
2024-08-18 13:36:26 -0500

    ADD: envs deps

---
+++ src/config/envs.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  DATABASE_URL: string;
+
+  NATS_SERVERS: string;
+}
+
+// validate envs with joi schema
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(), // ya lo parsea a number
+    DATABASE_URL: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+  })
+  .unknown(true); // allow other envs not defined in schema - process.env
+
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envsVars: EnvVars = value;
+
+export const envs = {
+  PORT: envsVars.PORT,
+  DATABASE_URL: envsVars.DATABASE_URL, // aunq no se use fuera d prisma, lo incluimos para q se valide
+
+  NATS_SERVERS: envsVars.NATS_SERVERS,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
+  // // envs
+  const PORT = envs.PORT;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
